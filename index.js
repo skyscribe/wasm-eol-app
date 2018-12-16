a984a07fc17bdb420b6a43715f24ebbca85afd4a
Fei Yan <fei.yan@nokia-sbell.com>
2018-12-16 14:05:50 +0800

    link main wasm

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-import * as wasm from "hello-wasm-pack";
+import { get_universe } from "wasm-game-of-life";
 
-wasm.greet();
+const pre = document.getElementById("game-of-life-canvas");
+const universe = get_universe();
+
+const renderLoop = () => {
+    pre.textContent = universe.render();
+    universe.tick();
+    requestAnimationFrame(renderLoop);
+}
+
+requestAnimationFrame(renderLoop);
